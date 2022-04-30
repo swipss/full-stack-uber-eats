31606535704011a3cc23c38826b084388d06223f
swipss <vassiljevsten1@gmail.com>
2022-04-30 20:32:52 +0300

    Add dish detail screen

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
+import DishDetailScreen from './src/screens/DishDetailScreen';
 import HomeScreen from './src/screens/HomeScreen';
 import RestaurantDetailsScreen from './src/screens/RestaurantDetailsScreen';
 
@@ -9,7 +10,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       {/* <HomeScreen /> */}
-      <RestaurantDetailsScreen />
+      {/* <RestaurantDetailsScreen /> */}
+      <DishDetailScreen />
       <StatusBar style="light" />
     </View>
   );
