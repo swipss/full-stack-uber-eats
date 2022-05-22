eaf593d00c66b1973a05bc794bc8e795d3751de0
swipss <vassiljevsten1@gmail.com>
2022-05-22 11:01:03 +0300

    Add authentiacion logic and change profile info

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,18 +1,22 @@
 import { createContext, useState, useEffect, useContext } from "react";
-import { Auth } from "aws-amplify";
+import { Auth, DataStore } from "aws-amplify";
+import { User } from "../models";
 
 const AuthContext = createContext({})
 
 const AuthContextProvider = ({children}) => {
     const [authUser, setAuthUser] = useState(null)
     const [dbUser, setDbUser] = useState(null)
+    const sub = authUser?.attributes?.sub
 
     useEffect(() => {
         Auth.currentAuthenticatedUser({ bypassCache: true }).then(setAuthUser)
     }, [])
     // console.log(authUser)
 
-    const sub = authUser?.attributes?.sub
+    useEffect(() => {
+        DataStore.query(User, (user) => user.sub('eq', sub)).then((users) => setDbUser(users[0]))
+    },[sub])
 
     return (
         <AuthContext.Provider value={{ authUser, dbUser, sub, setDbUser }}>
