b3302d2b70e1eed7e2d09f65c07a508ad2ddd853
swipss <vassiljevsten1@gmail.com>
2022-06-12 22:37:54 +0300

    add folder

--- src/components/OrderListItem/index.js
+++ src/components/OrderListItem/index.js
@@ -1,23 +1,35 @@
-import { View, Text, Image, Pressable } from 'react-native'
-import { useNavigation } from '@react-navigation/native'
-import React from 'react'
+import { View, Text, Image, Pressable } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import React from "react";
 
 const OrderListItem = ({ order }) => {
-
-  const navigation = useNavigation()
+  const navigation = useNavigation();
 
   return (
-    <Pressable onPress={() => navigation.navigate("Order")} style={{flexDirection: 'row', margin: 10, alignItems: 'center'}}>
+    <Pressable
+      onPress={() => navigation.navigate("Order")}
+      style={{ flexDirection: "row", margin: 10, alignItems: "center" }}
+    >
+      <Image
+        source={{ uri: order.Restaurant.image }}
+        style={{
+          width: "100%",
+          width: 75,
+          height: 75,
+          borderRadius: 10,
+          marginRight: 10,
+        }}
+      />
 
-        <Image source={{ uri : order.Restaurant.image}} style={{width: '100%', width: 75, height: 75, borderRadius: 10, marginRight: 10}}/>
-
-        <View>
-            <Text style={{fontWeight: '600', fontSize: 16}}>{order.Restaurant.name}</Text>
-            <Text style={{marginVertical: 5}}>4 items &#8226; $38.23</Text>
-            <Text>2 days ago &#8226; {order.status}</Text>
-        </View>
+      <View>
+        <Text style={{ fontWeight: "600", fontSize: 16 }}>
+          {order.Restaurant.name}
+        </Text>
+        <Text style={{ marginVertical: 5 }}>4 items &#8226; $38.23</Text>
+        <Text>2 days ago &#8226; {order.status}</Text>
+      </View>
     </Pressable>
-  )
-}
+  );
+};
 
-export default OrderListItem
+export default OrderListItem;
--- src/screens/BasketScreen/index.js
+++ src/screens/BasketScreen/index.js
@@ -1,93 +1,93 @@
-import { View, Text, StyleSheet, FlatList, Pressable } from 'react-native'
-import React, {useState} from 'react'
-import { AntDesign } from '@expo/vector-icons'
-import BasketDishItem from '../../components/BasketDishItem'
-import { useBasketContext } from '../../contexts/BasketContext'
-import { useOrderContext } from '../../contexts/OrderContext'
-
-
+import { View, Text, StyleSheet, FlatList, Pressable } from "react-native";
+import React, { useState } from "react";
+import { AntDesign } from "@expo/vector-icons";
+import BasketDishItem from "../../components/BasketDishItem";
+import { useBasketContext } from "../../contexts/BasketContext";
+import { useOrderContext } from "../../contexts/OrderContext";
 
 const BasketScreen = () => {
-
-    const {restaurant, basketDishes, totalPrice} = useBasketContext()
-    const {createOrder} = useOrderContext()
+  const { restaurant, basketDishes, totalPrice } = useBasketContext();
+  const { createOrder } = useOrderContext();
 
   return (
     <View style={styles.page}>
       <Text style={styles.title}>{restaurant?.name}</Text>
 
-        <Text style={{fontWeight: 'bold', marginTop: 20, fontSize: 20}}>Your items</Text>
+      <Text style={{ fontWeight: "bold", marginTop: 20, fontSize: 20 }}>
+        Your items
+      </Text>
 
-        <FlatList 
+      <FlatList
         data={basketDishes}
-        renderItem={({item}) => <BasketDishItem basketDish={item} />}
-        />
+        renderItem={({ item }) => <BasketDishItem basketDish={item} />}
+      />
 
       <View style={styles.separator} />
 
-    <Pressable style={styles.button}>
-        <Text style={styles.buttonText} onPress={createOrder}>Create order &#8226; ${totalPrice.toFixed(2)}</Text>
-    </Pressable>
-
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText} onPress={createOrder}>
+          Create order &#8226; ${totalPrice.toFixed(2)}
+        </Text>
+      </Pressable>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    page: {
-        flex: 1,
-        width: '100%',
-        paddingVertical: 30,
-        padding: 10
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: '600',
-        marginVertical: 10
-    },
-    description: {
-        color: 'grey'
-    },
-    nameText: {
-        fontWeight: '600',
-    },
-    priceText: {
-        marginLeft: 'auto',
-    },
-    separator: {
-        height: 1,
-        backgroundColor: 'lightgrey',
-        marginVertical: 10,
-    },
-    row: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        marginVertical: 15
-    },
-    quantity: {
-        fontSize: 25,
-        fontWeight: 'bold',
-        marginHorizontal: 20
-    },
-    button: {
-        backgroundColor: 'black',
-        marginTop: 'auto',
-        padding: 20,
-        alignItems: 'center',
-        borderRadius: 10
-    },
-    buttonText: {
-        color: 'white',
-        fontWeight: '600',
-        fontSize: 18
-    },
-    quantityContainer: {
-        backgroundColor: 'lightgrey',
-        paddingHorizontal: 7,
-        paddingVertical: 2,
-        marginRight: 10,
-        borderRadius: 3,
-    },
-})
+  page: {
+    flex: 1,
+    width: "100%",
+    paddingVertical: 30,
+    padding: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "600",
+    marginVertical: 10,
+  },
+  description: {
+    color: "grey",
+  },
+  nameText: {
+    fontWeight: "600",
+  },
+  priceText: {
+    marginLeft: "auto",
+  },
+  separator: {
+    height: 1,
+    backgroundColor: "lightgrey",
+    marginVertical: 10,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 15,
+  },
+  quantity: {
+    fontSize: 25,
+    fontWeight: "bold",
+    marginHorizontal: 20,
+  },
+  button: {
+    backgroundColor: "black",
+    marginTop: "auto",
+    padding: 20,
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "600",
+    fontSize: 18,
+  },
+  quantityContainer: {
+    backgroundColor: "lightgrey",
+    paddingHorizontal: 7,
+    paddingVertical: 2,
+    marginRight: 10,
+    borderRadius: 3,
+  },
+});
 
-export default BasketScreen
+export default BasketScreen;
